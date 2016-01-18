cad8962d831aff0127d0794998aadb57a85ecaa4
gm758 <griffinmichl@gmail.com>
2016-01-18 14:45:45 -0800

    implement dancer and blinky dancer classes

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -17,3 +17,16 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
   return blinkyDancer;
 };
+
+var BlinkyDancer = function(top, left, timeBetweenSteps){
+  Dancer.call(this, top, left, timeBetweenSteps);
+};
+
+BlinkyDancer.prototype.step = function() {
+  // call the old version of step at the beginning of any call to this new version of step
+  Dancer.prototype.step();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  this.$node.toggle();
+};
