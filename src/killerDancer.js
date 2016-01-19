6f963d23f5959c18d92989ae6dbd99b7b5baed9d
gm758 <griffinmichl@gmail.com>
2016-01-19 11:29:44 -0800

    fixed killer dancer deletions

--- src/killerDancer.js
+++ src/killerDancer.js
@@ -3,6 +3,7 @@ var KillerDancer = function(top, left, timeBetweenSteps){
   this.verticalMovement = Math.floor(Math.random()*5 + 1);             
   Dancer.call(this, top, left, 10);
   this.$node = $('<img class="dancer killerDancer" src="images/carlton.gif">');
+  Dancer.prototype.setPosition.call(this,top,left);
 };
 
 KillerDancer.prototype = Object.create(Dancer.prototype);
@@ -29,7 +30,15 @@ KillerDancer.prototype.step = function() {
 
 KillerDancer.prototype.collisionAction = function(otherDancer){
   if (this != otherDancer) {
-    otherDancer.$node.toggle();
+    window.dancers[otherDancer.$node.data('dancer')] = undefined;
+    otherDancer.$node.remove();    
+
+    var currentHeight = this.$node.height();
+    var newHeight = 100*(currentHeight*1.1)/$('body').height();
+    if (newHeight > 100) {
+      newHeight = 100;
+    }
+    this.$node.css({'height':newHeight+'%'});
   }
 };
 
@@ -42,8 +51,10 @@ KillerDancer.prototype.collisionCheck = function(otherDancers) {
 
   //loop though the dancers arry and check if any of them are within 5px
   for(var i = 0;i<otherDancers.length;i++){
-    if(distance(this,otherDancers[i])<50){
-      this.collisionAction(otherDancers[i]);
-    }
+    if(otherDancers[i]){
+      if(distance(this,otherDancers[i])<100){
+        this.collisionAction(otherDancers[i]);
+      }      
+    }    
   }
 };
